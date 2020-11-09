ab94472eab9804dcbd47533a188c62935c617172
Japroz Saini <sainijaproz@gmail.com>
2020-11-09 10:48:11 +0530

    Added CSS properties

--- src/Aurora.js
+++ src/Aurora.js
@@ -4,6 +4,12 @@ Author : Japroz Saini <sainijaproz@gmail.com>
 Date : 8th October 2020
 */
 
-function convert() {
-    console.log("heyo!");
-}
+//Dynamic coloring
+// function dynamicColoring() {
+// const element = document.querySelector(".bg:rgb(0, 255, 0)");
+// console.log(element);
+// }
+
+// Call all the functions when the DOM loads
+// document.querySelector("DOMContentLoaded", () => {
+// });
