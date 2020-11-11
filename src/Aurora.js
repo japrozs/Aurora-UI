a001d0993f427319acbe5eabeb74742f469d2964
Japroz Saini <sainijaproz@gmail.com>
2020-11-11 12:48:38 +0530

    (#001)Added navbar.scss file

--- src/Aurora.js
+++ src/Aurora.js
@@ -12,4 +12,48 @@ Date : 8th October 2020
 
 // Call all the functions when the DOM loads
 // document.querySelector("DOMContentLoaded", () => {
-// });
+// })
+
+//ALL THE FUNCTIONS FROM HERE ARE COMMENTED AS SOME OF THEM
+// ARE NOT TO BE EXECUTES IN THE BROWSER
+
+// const fs = require('fs');
+// for (let i = 0; i < 101; i++) {
+//     fs.appendFileSync('css.txt', `
+//         .mb-${i}{
+//             margin-bottom:${i}px;
+//         }
+//         .ml-${i}{
+//             margin-left:${i}px;
+//         }
+//         .mt-${i}{
+//             margin-top:${i}px;
+//         }
+//         .mr-${i}{
+//             margin-right:${i}px;
+//         }
+//         .pb-${i}{
+//             padding-bottom:${i}px;
+//         }
+//         .pl-${i}{
+//             padding-left:${i}px;
+//         }
+//         .pt-${i}{
+//             padding-top:${i}px;
+//         }
+//         .pr-${i}{
+//             padding-right:${i}px;
+//         }
+//         .m-${i}{
+//             margin:${i}px;
+//         }
+//         .p${i}{
+//             padding:${i}px;
+//         }
+//     `, 'utf-8');
+// }
+
+function toggleMobileMenu() {
+    document.querySelector('.nav-menu').classList.toggle('active')
+    document.querySelector('.mav-toggle').classList.toggle('active')
+}
